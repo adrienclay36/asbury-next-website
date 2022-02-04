f4d6613e4f054b04c3d415cda8c47ce4b3c23e8a
Adrien Clay <adrienclay36@gmail.com>
2022-02-03 17:17:19 -0700

    Hero buttons now transparent

--- components/ui/main-button.js
+++ components/ui/main-button.js
@@ -4,7 +4,7 @@ const MainButton = (props) => {
   return (
     <button
       type="button"
-      className={`px-7 py-3 bg-seaFoam-500 hover:bg-seaFoam-800 font-semibold tracking-widest border-2 shadow-md border-gray-300 text-white uppercase rounded-lg ${props.margin} ${styles.btn}`}
+      className={`px-7 py-3 hover:bg-seaFoam-800 font-semibold tracking-widest border-2 shadow-md border-gray-300 text-white uppercase rounded-lg ${props.margin} ${styles.btn}`}
     >
       {props.children}
     </button>
