17355d87dce03a2ed21b40926d18572dd2bba38f
Adrien Clay <adrienclay36@gmail.com>
2022-02-13 15:20:22 -0700

    Testing events Calendar

--- components/events/events-section.js
+++ components/events/events-section.js
@@ -1,20 +1,49 @@
-import React from 'react'
-import SectionHeader from '../ui/section-heading';
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import SectionHeader from "../ui/section-heading";
+import Link from "next/link";
 import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import dayGridPlugin from '@fullcalendar/daygrid';
+import googleCalendarPlugin from "@fullcalendar/google-calendar";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import PageLoading from '../PageLoading/PageLoading';
 const EventsSection = () => {
+  const [loading, setLoading] = useState(true);
+  const eventObject = { googleCalendarId: process.env.NEXT_PUBLIC_CALENDAR_ID}
+  
+  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 100)
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [])
+
+  const loadingDiv = (
+    <div style={{height: "90vh"}} className="max-w-screen-lg mx-auto flex flex-1 items-center justify-center">
+      <PageLoading/>
+    </div>
+  )
+
+  const googleCal = (
+    <FullCalendar
+          plugins={[googleCalendarPlugin, dayGridPlugin, interactionPlugin]}
+          googleCalendarApiKey={process.env.NEXT_PUBLIC_GOOGLE_API}
+          events={eventObject}
+          selectable
+        />
+  )
+
 
   return (
     <SectionHeader title="Events">
       <div className="w-5/6 mx-auto">
-
-      <FullCalendar plugins={[dayGridPlugin, interactionPlugin]} selectable />
+        {!loading  ? googleCal : loadingDiv}
       </div>
     </SectionHeader>
   );
-}
+};
 
-export default EventsSection
+export default EventsSection;
