777b8791e7fe70706d71468e8c6a4658d825f912
Adrien Clay <adrienclay36@gmail.com>
2022-02-15 11:59:58 -0700

    Styling info hero for later, not added to final layout as of now

--- components/home/info-section-two/hero-info.js
+++ components/home/info-section-two/hero-info.js
@@ -3,8 +3,20 @@ import styles from './hero-info.module.css';
 const HeroInfo = () => {
   return (
     <div className={styles['hero-image']}>
-        <div className="flex flex-1 justify-center items-center max-w-screen-lg">
-            <h1 className="text-8xl text-left text-white font-bold">Welcome To Asbury</h1>
+        <div className={styles['hero-text']}>
+
+          
+            <h1 className="text-4xl lg:text-8xl md:text-7xl text-left text-white font-bold mb-12">Traditional Service</h1>
+            <div>
+              <p className="text-4xl font-semibold mb-6 uppercase opacity-70">Sundays</p>
+
+              <div className="flex flex-1 flex-col lg:flex-row md:flex-row justify-center items-center">
+                <p className="m-4 text-2xl font-semibold">8:00AM</p>
+                <p className="m-4 text-4xl font-semibold">&</p>
+                <p className="m-4 text-2xl font-semibold">11:00AM</p>
+              </div>
+            </div>
+
         </div>
     </div>
   )
