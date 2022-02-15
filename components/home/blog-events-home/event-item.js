87d563f76173b2b4a8544c4091cb63b257879e97
Adrien Clay <adrienclay36@gmail.com>
2022-02-15 04:25:40 -0700

    Trying another method of time string converison to mountain for vercel

--- components/home/blog-events-home/event-item.js
+++ components/home/blog-events-home/event-item.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { getDateInfo } from "../../../utils/dates";
 import Link from 'next/link';
-const EventItem = ({ date, title, timeframe, start, end}) => {
+const EventItem = ({ date, title, start, end}) => {
     const { day, monthText } = getDateInfo(date);
-    const startTime = new Date(start).toLocaleTimeString("en-US", { hour: 'numeric', minute: '2-digit', timeZone: "America/Denver"});
-    const endTime = new Date(end).toLocaleTimeString("en-US", {
-      hour: "numeric",
-      minute: "2-digit",
-      timeZone: "America/Denver"
-    });
+    // const startTime = new Date(start).toLocaleTimeString("en-US", { hour: 'numeric', minute: '2-digit', timeZone: "America/Denver"});
+    // const endTime = new Date(end).toLocaleTimeString("en-US", {
+    //   hour: "numeric",
+    //   minute: "2-digit",
+    //   timeZone: "America/Denver"
+    // });
+
+
+    const calcTime = (date, offset) => {
+
+
+      const utc = date.getTime() + date.getTimezoneOffset() * 60000;
+
+      const nd = new Date(utc + 3600000 * offset);
+
+      return nd.toLocaleTimeString("en-US", {hour: "numeric", minute: "2-digit"});
+    }
+
+    const startTime = calcTime(new Date(start), -7);
+    const endTime = calcTime(new Date(end), -7);
     
   return (
     <Link href="/events" passHref>
@@ -18,8 +32,8 @@ const EventItem = ({ date, title, timeframe, start, end}) => {
         <h1 className="text-2xl uppercase">{day}</h1>
         <p className="uppercase">{monthText}</p>
       </div>
-      <div className="ml-8 mt-2 max-h-24">
-        <h1 className="uppercase font-semibold text-seaFoam-800 text-lg">
+      <div className="ml-8 mt-2">
+        <h1 className="uppercase font-semibold text-seaFoam-800 lg:text-lg md:text-lg">
           {title.length > 20 ? title.slice(0,28) + "..." : title}
         </h1>
         <h1 className="uppercase font-semibold text-seaFoam-600 text-md mt-4">
