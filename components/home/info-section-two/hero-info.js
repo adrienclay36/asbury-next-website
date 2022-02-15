7713cfed647e8c7e9caecfcfde384da0e510ca17
Adrien Clay <adrienclay36@gmail.com>
2022-02-15 13:41:11 -0700

    Playing with formatting on hero-info

--- components/home/info-section-two/hero-info.js
+++ components/home/info-section-two/hero-info.js
@@ -6,14 +6,14 @@ const HeroInfo = () => {
         <div className={styles['hero-text']}>
 
           
-            <h1 className="text-4xl lg:text-8xl md:text-7xl text-left text-white font-bold mb-12"><span className={`${styles.clip} bg-clip-text text-transparent`}>Traditional</span> Service</h1>
+            <h1 className="text-5xl lg:text-8xl md:text-7xl text-center text-white font-bold mb-12"><span className={`${styles.clip} bg-clip-text text-transparent`}>Traditional</span> Service</h1>
             <div>
-              <p className="text-4xl font-semibold mb-6 uppercase opacity-70">Sundays</p>
+              <p className={`${styles.clip} text-xl lg:text-4xl md:text-4xl font-semibold mb-6 uppercase bg-clip-text text-transparent`}>Sundays</p>
 
               <div className="flex flex-1 flex-col lg:flex-row md:flex-row justify-center items-center">
-                <p className="m-4 text-2xl font-semibold">8:00AM</p>
-                <p className="m-4 text-4xl font-semibold">&</p>
-                <p className="m-4 text-2xl font-semibold">11:00AM</p>
+                <p className="m-4 text-xl lg:text-2xl md:text-2xl font-semibold">8:00AM</p>
+                <p className="m-4 text-2xl lg:text-4xl md:text-4xl font-semibold">&</p>
+                <p className="m-4 text-xl lg:text-2xl md:text-2xl font-semibold">11:00AM</p>
               </div>
             </div>
 
