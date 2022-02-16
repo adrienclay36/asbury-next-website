dc0de0690661d867a91db6cf385be123944f16d9
Adrien Clay <adrienclay36@gmail.com>
2022-02-16 13:02:02 -0700

    Adding back joys and concerns for use with live database changes from supabase

---
+++ pages/joys-and-concerns/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import JoysAndConcernsSection from '../../components/joys-and-concerns/joys-and-concerns-section';
+import Layout from '../../components/layout/layout';
+const JoysAndConcerns = () => {
+  return (
+    <Layout title="Joys and Concerns">
+        <JoysAndConcernsSection/>
+    </Layout>
+  )
+}
+
+export default JoysAndConcerns
