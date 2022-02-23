e8e0b149c7a129a6246d177a4544a458072a5e79
Adrien Clay <adrienclay36@gmail.com>
2022-02-22 19:28:55 -0700

    Adding links in _document.js <Head> tag for google fonts, they're not persisting across machines

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,32 @@
 import Document from "next/document";
 import { createGetInitialProps } from "@mantine/next";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
   static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/> */}
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400;500;600&display=swap" rel="stylesheet"/>
+          </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+  
+
