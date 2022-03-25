1f620f3c5839d4cb5a1bbc5589f860ce04141e8f
Adrien Clay <adrienclay36@gmail.com>
2022-03-25 07:26:13 -0600

    Fixing checkout-success page width

--- pages/giving/checkout-success.js
+++ pages/giving/checkout-success.js
@@ -6,7 +6,7 @@ const CheckoutSuccess = () => {
       <AiOutlineCheckCircle size={100} className="text-green-600" />
       <p className="my-4 font-bold text-xl text-center">Checkout Successful!</p>
       <p className="font-semibold text-xl text-center">Thank you for your donation!</p>
-      <p className="font-semibold text-xl text-center">A record for this transaction will appear in your app shortly.</p>
+      <p className="font-semibold text-xl text-center w-11/12 mx-auto">A record for this transaction will appear in your app shortly.</p>
     </div>
   );
 }
