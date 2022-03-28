379910bebb35a4d6b221bcee74e26c0caf2e0255
Adrien Clay <adrienclay36@gmail.com>
2022-03-27 19:31:57 -0600

    Changing redirect for password reset form because mobile users will be using it

--- components/admin/forgot-pass-form.js
+++ components/admin/forgot-pass-form.js
@@ -37,7 +37,7 @@ const ForgotPasswordForm = () => {
                 setSuccess(true);
                 setSending(false);
                 setTimeout(() => {
-                  router.push("/admin");
+                  router.push("/");
                 }, 3000)
                 return;
             } catch(err) {
