97bd8246d130268af1ac30d7110ea731e75f0c05
Adrien Clay <adrienclay36@gmail.com>
2022-04-02 08:40:24 -0600

    add meta tag for non-scalable so input on mobile doesn't zoom in

--- pages/_app.js
+++ pages/_app.js
@@ -24,13 +24,16 @@ function MyApp({ Component, pageProps }) {
         />
         <title>Asbury UMC</title>
         <link rel="icon" href="/images/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
       </Head>
       <NextNProgress height={4} color={"#899e9c"} />
 
-        <UserContextProvider>
-          <Component {...pageProps} />
-        </UserContextProvider>
-
+      <UserContextProvider>
+        <Component {...pageProps} />
+      </UserContextProvider>
     </>
   );
 }
