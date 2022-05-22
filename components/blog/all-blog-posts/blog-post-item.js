2d72a805835882f21ec2a9960b8caa3b0a29829d
Adrien Clay <adrienclay36@gmail.com>
2022-05-22 13:45:36 -0600

    Replacing CKEditor to include autolink and updating text slicing on blog post items

--- components/blog/all-blog-posts/blog-post-item.js
+++ components/blog/all-blog-posts/blog-post-item.js
@@ -17,22 +17,27 @@ const BlogPostItem = ({ post, i }) => {
     .toLowerCase();
 
   const getPreviewText = (inputContent) => {
-    if (inputContent.length < 150) {
-      const firstCutoff = inputContent.indexOf("</p>");
-      const previewText = inputContent.slice(0, firstCutoff + 4);
-      return previewText;
-    }
+      if(inputContent.indexOf("</p>") > 50) {
+        
+        return inputContent.slice(0, 50) + "...</p>";
+      }
+      if (inputContent.length < 150) {
+        const firstCutoff = inputContent.indexOf("</p>");
+        const previewText = inputContent.slice(0, firstCutoff + 4);
+        return previewText;
+      }
 
     if (inputContent.length > 150) {
       const slicedContent = inputContent.slice(0, 150) + "...</p>";
       return slicedContent;
     }
   };
+  console.log(post?.postcontent);
 
   return (
     <div className={styles[`post-animation-${i}`]}>
       <div className={`border-2 shadow-lg w-11/12 lg:w-full md:w-full mx-auto`}>
-        <img className={`object-cover w-full h-64`} src={post?.image} />
+        <img alt={post?.title} className={`object-cover w-full h-64`} src={post?.image} />
         <div>
           <div className="pt-10 px-10 flex flex-1 justify-center lg:justify-between md:justify-between items-center">
             <h1 className={`text-2xl text-center lg:text-left md:text-center`}>
@@ -59,10 +64,11 @@ const BlogPostItem = ({ post, i }) => {
         </div>
         <div className="h-0.5 w-3/6 rounded-lg bg-gray-200 mx-auto my-5"></div>
         <div className="px-10 pb-5">
-          {post?.postcontent.length > 50 && parse(getPreviewText(post?.postcontent))}
+          {post?.postcontent.length > 50 &&
+            parse(getPreviewText(post?.postcontent))}
           {post?.postcontent.length < 50 && parse(post?.postcontent)}
         </div>
-        <div className="flex justify-center lg:justify-end md:justify-end items-center p-4">
+        <div className="flex justify-center lg:justify-end md:justify-center items-center p-4">
           <button
             onClick={() => router.push(`/blog/${post?.id}/${formatTitle}`)}
             className="px-7 py-3 m-2 rounded-md bg-seaFoam-400 text-white uppercase font-semibold tracking-wide"
