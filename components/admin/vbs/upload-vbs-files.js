d6954489471e23bc0432a90b8b4b531b8724cbc0
Adrien Clay <adrienclay36@gmail.com>
2022-05-23 18:58:58 -0600

    Adding progress bar for uploads

--- components/admin/vbs/upload-vbs-files.js
+++ components/admin/vbs/upload-vbs-files.js
@@ -2,30 +2,39 @@ import React, { useRef, useState } from "react";
 import AsburyButton from "../../ui/AsburyButton";
 import { BsFillTrash2Fill } from "react-icons/bs";
 import { supabase } from "../../../supabase-client";
+import { Progress } from "@mantine/core";
 const UploadVBSFiles = ({ existingFiles, getFiles }) => {
   const [files, setFiles] = useState();
   const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
   const inputRef = useRef(null);
   const uploadFiles = async () => {
-      setLoading(true);
-    for (let i = 0; i < files.length; i++) {
-      const { data, error } = await supabase.storage
-        .from("vbs-files")
-        .upload(`2022/${files[i].name}`, files[i]);
-      if (error) {
-        alert(
-          `Error uploading file: ${files[i]?.name}\n\nThe error was caused by: ${error?.message}`
-        );
+    setLoading(true);
+    if (files) {
+      const progressTotal = files.length;
+
+      for (let i = 0; i < files.length; i++) {
+        const { data, error } = await supabase.storage
+          .from("vbs-files")
+          .upload(`2022/${files[i].name}`, files[i]);
+        if (error) {
+          alert(
+            `Error uploading file: ${files[i]?.name}\n\nThe error was caused by: ${error?.message}`
+          );
+        }
+
+        setProgress((i + 1 / progressTotal) * 100);
       }
+      await getFiles();
+      inputRef.current.value = null;
+      setFiles(null);
+      setProgress(0);
     }
-    await getFiles();
-    inputRef.current.value = null;
-    setFiles(null);
     setLoading(false);
   };
 
   const deleteFile = async (fileName) => {
-      setLoading(true);
+    setLoading(true);
     const confirmation = confirm(
       "Are you sure you want to delete this file? This action cannot be undone."
     );
@@ -48,15 +57,26 @@ const UploadVBSFiles = ({ existingFiles, getFiles }) => {
     <>
       <div className="flex flex-1 flex-col justify-center items-center">
         <input
-            ref={inputRef}
+          ref={inputRef}
           multiple
           className="my-6"
           onChange={(e) => setFiles(e.target.files)}
           type="file"
           required
         />
-        <AsburyButton disabled={!files} loading={loading} text="Upload Files" onClick={uploadFiles} />
+        <AsburyButton
+          disabled={!files}
+          loading={loading}
+          text="Upload Files"
+          onClick={uploadFiles}
+        />
       </div>
+      <Progress
+        animate
+        striped
+        className="my-8 w-11/12 lg:w-3/6 md:w-3/6 mx-auto"
+        value={progress}
+      />
       <div>
         {existingFiles.map((file) => (
           <div
@@ -64,12 +84,12 @@ const UploadVBSFiles = ({ existingFiles, getFiles }) => {
             key={file?.name}
           >
             <p>{file?.name}</p>
-            <BsFillTrash2Fill
-            
-              onClick={() => deleteFile(file?.name)}
-              size={20}
-              className="text-red-600 cursor-pointer hover:text-red-700"
-            />
+            <button disabled={loading} onClick={() => deleteFile(file?.name)}>
+              <BsFillTrash2Fill
+                size={20}
+                className="text-red-600 cursor-pointer hover:text-red-700"
+              />
+            </button>
           </div>
         ))}
       </div>
