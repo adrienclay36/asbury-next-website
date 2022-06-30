d90c9287fd8ddf7bcdd48b67978f2b60a77ff67d
Adrien Clay <adrienclay36@gmail.com>
2022-06-30 13:39:41 -0600

    Adding trails route

---
+++ components/asbury-trails/trails-list.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { supabase } from '../../supabase-client'
+const TrailsList = () => {
+  return (
+    <div>TrailsList</div>
+  )
+}
+
+export default TrailsList
---
+++ pages/asbury-trails/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Layout from '../../components/layout/layout';
+import TrailsList from '../../components/asbury-trails/trails-list';
+import SectionHeading from '../../components/ui/section-heading';
+import { supabase } from '../../supabase-client';
+const AsburyTrailsHome = ({ files }) => {
+    return (
+        <Layout title="Asbury Trails" description="The official monthly newsletter for Asbury United Methodist Church, where you can find information regarding all the latest events and news around the community.">
+            <SectionHeading title="Asbury Trails">
+                <TrailsList />
+            </SectionHeading>
+        </Layout>
+    )
+}
+
+export default AsburyTrailsHome
+
+export const getStaticProps = async ({ req, res }) => {
+    const {data, error} = await supabase.storage.from('trails').list('trails');
+    console.log(data);
+    return {
+        props: {
+            files: []
+        }
+    }
+}
