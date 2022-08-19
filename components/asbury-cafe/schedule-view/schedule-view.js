f0c8b4b2ed58a20c1697f41a96349b122620942e
Adrien Clay <adrienclay36@gmail.com>
2022-08-18 20:00:40 -0600

    Adding training video and reset password links into InfoDropdown components instead of buttons

--- components/asbury-cafe/schedule-view/schedule-view.js
+++ components/asbury-cafe/schedule-view/schedule-view.js
@@ -1,33 +1,39 @@
-import React from "react";
-import parse from "html-react-parser";
+import React, { useRef } from "react";
 import AsburyButton from "../../ui/AsburyButton";
 import { useRouter } from "next/router";
-
+import InfoDropdown from '../../../components/ui/info-dropdown/info-dropdown';
+import SectionHeading from "../../ui/section-heading";
 const ScheduleView = () => {
   const router = useRouter();
+  const videoRef = useRef(null);
 
-  const linkToCoordinator = () => {
-    router.push("");
-  };
   return (
     <div>
-      <div className="text-center my-6">
-        <a
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-0 w-11/12 lg:w-4/6 mx-auto">
+        <InfoDropdown
+          title={"Coordinator Sign In"}
+          buttonText="Sign In"
           href="https://c0abi487.caspio.com/dp/eb2a50002a387c33d89944eba2b6"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <AsburyButton text="Coordinator Sign-In" />
-        </a>
-      </div>
-      <div className="text-center my-6">
-        <a
+          content="Sign In as a coordinator to work on the Asbury Cafe schedule."
+        />
+
+        <InfoDropdown
+          title="Coordinator Registration"
+          buttonText="Register"
           href="https://c0abi487.caspio.com/dp/eb2a5000095791f32aae45e08424"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <AsburyButton text="Coordinator Registration" />
-        </a>
+          content="Sign Up here if you were designated as a coordinator for the Asbury Pie Cafe"
+        />
+      </div>
+      <div className="w-11/12 lg:w-2/6 md:w-4/6 mx-auto mb-12">
+        <InfoDropdown
+          title="Forgot Password?"
+          buttonText="Reset Password"
+          href="https://c0abi487.caspio.com/dp/eb2a500034d721ee5f8b40bca52f"
+          content="Follow the below link to reset your password. It will require your username and email"
+        />
+      </div>
+      <div className="text-center">
+        <AsburyButton onClick={() => videoRef.current.scrollIntoView({ behavior: 'smooth' })} text="Pie Cafe Training Video" />
       </div>
       <p className="text-center text-lg mt-4">
         Select a date below to search the existing schedule for the Cafe
@@ -41,6 +47,20 @@ const ScheduleView = () => {
           Sorry, but your browser does not support frames.
         </iframe>
       </div>
+      <SectionHeading reactRef={videoRef} title="Pie Cafe Training Video" >
+
+      <div className="container w-full" >
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/_ElHpZ52wbg"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      </SectionHeading>
     </div>
   );
 };
--- components/ui/section-heading.js
+++ components/ui/section-heading.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
-const SectionHeading = (props) => {
+const SectionHeading = ({ title, subheading, children, reactRef = null }) => {
   return (
-    <section id="features" className="bg-gray-100 py-12">
-      {props.title && <div className="text-center flex flex-1 justify-center">
-        <div className="h-1 w-60 rounded-lg bg-gray-400 mx-10 mt-5"></div>
-        <h1 className="uppercase text-lg lg:text-3xl md:text-2xl mt-2 lg:mt-0 md:mt-1">{props.title}</h1>
-        <div className="h-1 w-60 rounded-lg bg-gray-400 mx-10 mt-5"></div>
-      </div>}
+    <section ref={reactRef} id={title} className="bg-gray-100 py-12">
+      {title && (
+        <div className="text-center flex flex-1 justify-center">
+          <div className="h-1 w-60 rounded-lg bg-gray-400 mx-10 mt-5"></div>
+          <h1 className="uppercase text-lg lg:text-3xl md:text-2xl mt-2 lg:mt-0 md:mt-1">
+            {title}
+          </h1>
+          <div className="h-1 w-60 rounded-lg bg-gray-400 mx-10 mt-5"></div>
+        </div>
+      )}
 
       <div className="text-center text-seaFoam-500">
-        <h1 className="text-md lg:text-4xl md:text-2xl mt-12 uppercase tracking-widest">{props.subheading || null}</h1>
+        <h1 className="text-md lg:text-4xl md:text-2xl mt-12 uppercase tracking-widest">
+          {subheading || null}
+        </h1>
       </div>
-      {props.children}
+      {children}
     </section>
   );
 };
